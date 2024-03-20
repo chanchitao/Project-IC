c7f97e0b237407875794401be2bdeb323f2b139a
chanchitao <chanchitao@gmail.com>
2024-03-20 17:02:53 +0000

    add some comments

--- helloWorld.py
+++ helloWorld.py
@@ -7,10 +7,13 @@ print('Enter your mobile no.')
 z = input() 
 print('Enter your post code.')
 postcode = input() 
+### Input City
 print('Enter your City.')
 City = input() 
+### Input Address 
 print('Enter your Address.')
 Address = input() 
+
 print('Hello, ' + x + " "+ y)
 print('Your Mobile No. is ' + z)
 print('Your Post Code is ' + postcode)
