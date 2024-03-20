1493db6bf478ef6318eb25a91deb5ee189895587
chanchitao <chanchitao@gmail.com>
2024-03-20 18:02:37 +0000

    first commit

--- helloWorld.py
+++ helloWorld.py
@@ -7,14 +7,15 @@ print('Enter your mobile no.')
 z = input() 
 print('Enter your post code.')
 postcode = input() 
+
 ### Input City
-print('Enter your City.')
-City = input() 
+##print('Enter your City.')
+##City = input() 
 ### Input Address 
-print('Enter your Address.')
-Address = input() 
+##print('Enter your Address.')
+##Address = input() 
 
 print('Hello, ' + x + " "+ y)
 print('Your Mobile No. is ' + z)
 print('Your Post Code is ' + postcode)
-print('You are living at ' + Address + " "+ City )
+##print('You are living at ' + Address + " "+ City )
